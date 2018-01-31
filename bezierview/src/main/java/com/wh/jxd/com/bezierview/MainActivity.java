ccce22701646636896b47619ee58f08f91c47a30
kevin321 <kevin321vip@126.com>
2018-01-31 19:38:02 +0800

    updata

--- bezierview/src/main/java/com/wh/jxd/com/bezierview/MainActivity.java
+++ bezierview/src/main/java/com/wh/jxd/com/bezierview/MainActivity.java
@@ -1,7 +1,12 @@
 package com.wh.jxd.com.bezierview;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -9,7 +14,7 @@ import android.widget.LinearLayout;
 import com.wh.jxd.com.bezierview.widget.HweatherWidget;
 import com.wh.jxd.com.bezierview.widget.PullViscousView;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, NavigationView.OnNavigationItemSelectedListener {
 
     private LinearLayout mLl_root;
     private PullViscousView mPull_viscous_view;
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private float mStartX;
     private float mStartY;
     private HweatherWidget mHweatherWidget;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
         mLl_root = (LinearLayout) findViewById(R.id.ll_root);
         mPull_viscous_view = (PullViscousView) findViewById(R.id.pull_viscousview);
+        mDrawerLayout = findViewById(R.id.draw_layout);
         mHweatherWidget = (HweatherWidget) findViewById(R.id.weather);
-        mLl_root.setOnTouchListener(this);
+        mNavigationView = findViewById(R.id.nv_main_navigation);
 
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mLl_root.setOnTouchListener(this);
         //设置当前时间模拟早上6点
         mHweatherWidget.setCurrentTime(300);
     }
 
     /**
      * 根布局的触摸监听
+     *
      * @param v
      * @param event
      * @return
@@ -64,5 +75,29 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 break;
         }
         return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.nav_one:
+                intent = new Intent(this, ProgressPlateActivtiy.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_two:
+                intent=new Intent(this,BezierViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_three:
+                break;
+            case R.id.nav_four:
+                break;
+            case R.id.nav_five:
+                break;
+        }
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
+        return true;
     }
 }
