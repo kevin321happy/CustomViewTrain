3dc2024639553cfc89c012335daf00a127ea675f
kevin321 <kevin321vip@126.com>
2018-02-01 21:17:38 +0800

    扫描效果的绘制

--- bezierview/src/main/java/com/wh/jxd/com/bezierview/widget/CheckVirusView.java
+++ bezierview/src/main/java/com/wh/jxd/com/bezierview/widget/CheckVirusView.java
@@ -1,13 +1,18 @@
 package com.wh.jxd.com.bezierview.widget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 /**
  * Created by kevin321vip on 2018/2/1.
@@ -33,7 +38,30 @@ public class CheckVirusView extends View {
      * 画笔的颜色
      */
     private int mPaintColor = Color.BLACK;
+    /**
+     * 旋转的角度
+     */
+    private int mAngle;
+    /**
+     * 矩阵
+     */
+    private Matrix mMatrix = new Matrix();
     private Path mPath;
+    /**
+     * 动画的值
+     */
+    private int mValue;
+    /**
+     * 绘制扫描的画笔
+     */
+    private Paint mGradientPaint;
+    private SweepGradient mShader;
+    private int mShaderColor=Color.BLUE;
+    private ValueAnimator mAnimator;
+    /**
+     * 扫描画笔的透明度不断变化
+     */
+    private int mScanAlpha;
 
     public CheckVirusView(Context context) {
         super(context);
@@ -59,9 +87,12 @@ public class CheckVirusView extends View {
         mCPointy = heightSize / 2;
         mRadius = widthSize / 2 - mStrokeWidth;
         initPathTrack();
+        mAnimator.start();
     }
+
     /**
      * 初始化
+     *
      * @param context
      * @param attrs
      */
@@ -71,9 +102,38 @@ public class CheckVirusView extends View {
         mPaint.setDither(true);
         mPaint.setColor(mPaintColor);
         mPaint.setStyle(Paint.Style.STROKE);
+        //十字路径
         mPath = new Path();
 
+        //绘制扫描的画笔
+        mGradientPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mGradientPaint.setAntiAlias(true);
+        mGradientPaint.setDither(true);
+        mGradientPaint.setStyle(Paint.Style.FILL);
+//        mGradientPaint.setStrokeWidth(mRadius);
+        initAnimation();
 
+
+    }
+
+    /**
+     * 初始化动画
+     */
+    private void initAnimation() {
+        mAnimator = ValueAnimator.ofInt(0, 360);
+        //动画插值器
+        mAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mValue = (int) animation.getAnimatedValue();
+                Log.i("value","获取到了动画的值:"+mValue);
+                postInvalidate();
+            }
+        });
+        mAnimator.setDuration(1500);
+//        mAnimator.setRepeatMode(ValueAnimator.RESTART);
+        mAnimator.setRepeatCount(100);
 
     }
 
@@ -101,8 +161,33 @@ public class CheckVirusView extends View {
         super.onDraw(canvas);
         drawCircle(canvas);
         drawCrossPath(canvas);
-//        canvas.drawCircle();
+        drawScan(canvas);
+    }
 
+    /**
+     * 绘制扫描的效果
+     *
+     * @param canvas
+     */
+    private void drawScan(Canvas canvas) {
+        //设置透明度
+//        mGradientPaint.setAlpha(mScanAlpha);
+        mScanAlpha -= 5;
+        if (mScanAlpha < 0) {
+            mScanAlpha = 30;
+        }
+        Log.i("value","旋转的rotate:"+mValue);
+        //绘制雷达扫描的效果
+        //绘制雷达扫描的效果
+        mMatrix.setRotate(mValue, mCPointx, mCPointy);
+        if (mShader == null) {
+            //渐变效果
+            mShader = new SweepGradient(mCPointx, mCPointy, Color.TRANSPARENT, mShaderColor);
+        }
+        mShader.setLocalMatrix(mMatrix);
+        mGradientPaint.setShader(mShader);
+        //绘制扫描效果
+        canvas.drawCircle(mCPointx, mCPointy, mRadius , mGradientPaint);
     }
 
     /**
@@ -111,7 +196,7 @@ public class CheckVirusView extends View {
      * @param canvas
      */
     private void drawCrossPath(Canvas canvas) {
-        mPaint.setStrokeWidth(mStrokeWidth/2);
+        mPaint.setStrokeWidth(mStrokeWidth / 2);
         canvas.drawPath(mPath, mPaint);
     }
 
