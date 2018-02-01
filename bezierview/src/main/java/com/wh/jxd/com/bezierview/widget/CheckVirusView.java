b62a91ab966100ea704717fe0c689ce205dc0b74
kevin321 <kevin321vip@126.com>
2018-02-01 19:24:42 +0800

    checkvirusViewi

---
+++ bezierview/src/main/java/com/wh/jxd/com/bezierview/widget/CheckVirusView.java
@@ -0,0 +1,25 @@
+package com.wh.jxd.com.bezierview.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kevin321vip on 2018/2/1.
+ * 仿华为病毒检查控件
+ */
+
+public class CheckVirusView extends View {
+    public CheckVirusView(Context context) {
+        super(context);
+    }
+
+    public CheckVirusView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckVirusView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
